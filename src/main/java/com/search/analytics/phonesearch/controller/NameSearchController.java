3745a7e79670780b84d25b768ce9e2dc4d03c146
Venkata Siva Kumar Kottana <k.venkatshiv@gmail.com>
2021-01-04 13:27:15 +0530

    CORS enabled at server side

--- src/main/java/com/search/analytics/phonesearch/controller/NameSearchController.java
+++ src/main/java/com/search/analytics/phonesearch/controller/NameSearchController.java
@@ -1,6 +1,7 @@
 package com.search.analytics.phonesearch.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import java.util.List;
 
 import com.search.analytics.phonesearch.service.NameSearchService;
 
+@CrossOrigin("*")
 @RestController
 public class NameSearchController {
 
@@ -15,7 +17,7 @@ public class NameSearchController {
 	NameSearchService nameSearchService;
 
 	@RequestMapping("/names")
-	public List<String> getAllNames(@RequestParam("data") String data) {
+	public List<String> getAllNames(@RequestParam("input") String data) {
 		return nameSearchService.getAllNames(data);
 	}
 }
